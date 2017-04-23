139a02f4478eafce8b684306e3aa5fc8ff718fe1
abdi darmawan <abdid46@gmail.com>
2017-04-23 14:33:46 +0700

    test test

--- code-app/index.php
+++ code-app/index.php
@@ -3,6 +3,6 @@
   <title>Web Docker</title>
  </head>
  <body>
- <?php echo '<p>Hai Test Docker at AWS, keren gan, v-20</p>'; ?>
+ <?php echo '<p>Hai Test Docker at AWS, keren gan, v-21</p>'; ?>
  </body>
 </html>

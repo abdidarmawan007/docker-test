56adddedb53359bb0724af3a37bb0ff3159b9343
abdi darmawan <abdid46@gmail.com>
2017-04-24 12:01:18 +0700

    fix bug alient

--- code-app/index.php
+++ code-app/index.php
@@ -3,6 +3,6 @@
   <title>Web Docker</title>
  </head>
  <body>
- <?php echo '<p>Hai Test Docker at AWS, keren gan, v-27</p>'; ?>
+ <?php echo '<p>Hai Test Docker at AWS, keren gan, v-28</p>'; ?>
  </body>
 </html>

60e89719768fb1cd13cee9c4d43ad9769c59c719
abdi darmawan <abdid46@gmail.com>
2017-04-24 12:16:51 +0700

    change banner

--- code-app/index.php
+++ code-app/index.php
@@ -3,6 +3,6 @@
   <title>Web Docker</title>
  </head>
  <body>
- <?php echo '<p>Hai Test Docker at AWS, keren gan,, v-28</p>'; ?>
+ <?php echo '<p>Hai Test Docker at AWS, keren gan,, v-29</p>'; ?>
  </body>
 </html>

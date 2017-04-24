e785796453900d7e6264ef4e9418cd625208b3e4
abdi darmawan <abdid46@gmail.com>
2017-04-24 11:49:19 +0700

    fix bug from hell

--- code-app/index.php
+++ code-app/index.php
@@ -3,6 +3,6 @@
   <title>Web Docker</title>
  </head>
  <body>
- <?php echo '<p>Hai Test Docker at AWS, keren gan, v-26</p>'; ?>
+ <?php echo '<p>Hai Test Docker at AWS, keren gan, v-27</p>'; ?>
  </body>
 </html>
